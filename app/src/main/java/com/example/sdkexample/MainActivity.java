1e7fcaf19848d0acee5a371912a95138708082c4
andyesgame <andy@esgame.vn>
2022-04-27 17:25:09 +0700

    readme

--- app/src/main/java/com/example/sdkexample/MainActivity.java
+++ app/src/main/java/com/example/sdkexample/MainActivity.java
@@ -53,22 +53,25 @@ public class MainActivity extends Activity implements ESGameCallback {
         ESGameSDK.getInstance().handleIntent(intent);
     }
 
-    String productID = "com.userjoy.sgc.vinn_yuanbao01";
+    String productID = "vn.esgame.bltt_gp_zhichonglibao2_00229";
+    String productIDWeb = "vn.esgame.bltt_web_gold_00020";
+    String serverId = "1";
 
     private void initView() {
         ESGameSDK.init(this, this);
+        ESGameSDK.getInstance().setSandBox(true);
         ESGameSDK.getInstance().handleIntent(getIntent());
 
         btnBilling.setOnClickListener(view -> {
                     // ESGameSDK.getInstance().showWebSupportPage();
 //            ESGameSDK.getInstance().inAppBilling(productID,"test", "1234","extra_data");
-                    ESGameSDK.getInstance().inAppBillingWithSkuType(productID, BillingClient.SkuType.INAPP, "1", "1234", "extra_data");
+                    ESGameSDK.getInstance().inAppBillingWithSkuType(productID, BillingClient.SkuType.INAPP, serverId, "1234", "extra_data");
 //            ESGameSDK.getInstance().inAppBilling(productID, "test", "1234","extra_data","{}");
 
                 }
         );
 
-        btnBillingWeb.setOnClickListener(view -> ESGameSDK.getInstance().inAppBillingWeb("vnpay_4750", "formal", "1234", UUID.randomUUID().toString()));
+        btnBillingWeb.setOnClickListener(view -> ESGameSDK.getInstance().inAppBillingWeb(productIDWeb, serverId, "1234", UUID.randomUUID().toString()));
 
         login.setOnClickListener(view -> ESGameSDK.getInstance().login());
 
